2599545a7b8e0f11811ea53b3e92d16bbf0b9844
Oliver Erxleben <oliver.erxleben@me.com>
2019-06-15 11:55:49 +0200

    setup: integrated mongodb-stitch as angular service
    added test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StitchService } from './services/stitch.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'foodlensemvp';
+
+  constructor(private stitch: StitchService) {
+    this.stitch.init();
+  }
 }
--- src/app/services/stitch.service.ts
+++ src/app/services/stitch.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { Stitch, AnonymousCredential } from 'mongodb-stitch-browser-sdk';
+
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class StitchService {
 
-  constructor() { }
+  private client = Stitch.initializeDefaultAppClient('foodlense-api-ctyle');
+
+  constructor() {
+    console.log('stitch constructor');
+  }
+
+  public init() {
+    this.client.auth.loginWithCredential(new AnonymousCredential());
+  }
 }

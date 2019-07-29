3816bc583e843a64059a69a1052edf5ab6dcc35b
fabian <fabbianngala@gmail.com>
2019-07-29 06:27:59 +0300

    Add files via upload
    ui and home sreen

--- app.module.ts
+++ app.module.ts
@@ -1,66 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { ReactiveFormsModule }    from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
-import { MaterialModule} from './material';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatTabsModule} from '@angular/material/tabs';
-import { CameraComponent } from './camera/camera.component';
-import { ComponentsComponent } from './components/components.component';
-import { MapComponent } from './map/map.component';
-
-import { StartscreenComponent } from './startscreen/startscreen.component';
-
-import { AlertComponent } from './alert';
-
-
-import { routing }        from './app.routing.module';
-import { AuthGuard } from './guards';
-import { JwtInterceptor, ErrorInterceptor } from './helpers';
-import { AlertService, AuthenticationService, UserService } from './services';
-import { HomeComponent } from './home';
-import { LoginComponent } from './login';
-import { RegisterComponent } from './register';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material';
+import {ScrollingModule} from '@angular/cdk/scrolling';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CameraComponent,
-    ComponentsComponent,
-    MapComponent,
-    LoginComponent,
-    StartscreenComponent,
-    RegisterComponent,
-    AlertComponent,
-    HomeComponent,
+    AppComponent
   ],
   imports: [
-    BrowserAnimationsModule,
-    MaterialModule,
+  
+   MaterialModule,
+   ScrollingModule,
+  BrowserAnimationsModule,
     BrowserModule,
-
+    AppRoutingModule,
     IonicModule.forRoot(),
-    StoreModule.forRoot(reducers, { metaReducers }),
-    ReactiveFormsModule,
-    HttpClientModule,
-    routing
+    StoreModule.forRoot(reducers, { metaReducers })
   ],
-  providers: [
-    AuthGuard,
-    AlertService,
-    AuthenticationService,
-    UserService,
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }

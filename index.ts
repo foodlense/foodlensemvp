9d3ef80f2c00939b0ebaed66dcd1d6913f639696
fabian <fabbianngala@gmail.com>
2019-07-29 06:22:16 +0300

    Add files via upload
    authentication service

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
-export * from './error.interceptor';
-export * from './jwt.interceptor';
+export * from './alert.service';
+export * from './authentication.service';
+export * from './user.service';
